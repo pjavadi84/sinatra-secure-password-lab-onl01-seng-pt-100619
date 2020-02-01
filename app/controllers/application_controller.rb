b7033ddc5f479a02448a3e71a623c6a219e3f9c9
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-02-01 22:02:58 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    @user = User.new(:username => params[:username], :password => params[:password])
+    if params[:username].empty? || params[:password].empty?
+      redirect "/failure"
+    else
+      @user.save
+      redirect "/login"
+    end
   end
 
   get '/account' do
@@ -33,6 +38,16 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+     user = User.find_by(:username => params[:username])
+     
+    if params[:username].empty? || params[:password].empty?
+      redirect '/failure'
+    else
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect '/account'
+      end
+    end
   end
 
   get "/failure" do
